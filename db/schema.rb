dba739ec8f5fb23d181cf317a278aef8b82895ea
Jessica Nash <jessicanashacc@gmail.com>
2022-08-06 00:34:54 -0500

    remove date from journal entry table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_06_021541) do
+ActiveRecord::Schema.define(version: 2022_08_06_053256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2022_08_06_021541) do
   create_table "journal_entries", force: :cascade do |t|
     t.string "name"
     t.text "content"
-    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
