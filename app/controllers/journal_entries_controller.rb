d3ef7df6563f283ce7a0dc7d7b7e3cab071347a0
Jessica Nash <jessicanashacc@gmail.com>
2022-08-09 07:06:35 -0500

    created new form

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,2 +1,24 @@
 class JournalEntriesController < ApplicationController
+  def new
+    @journal_entry = JournalEntry.new
+    authorize @journal_entry
+  end
+
+  def create
+    @journal_entry = JournalEntry.new(journal_entry_params)
+    @journal_entry.user = current_user
+    authorize @journal_entry
+
+    if @journal_entry.save
+      redirect_to journal_entries_path, notice: 'New journal entry was written!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def journal_entry_params
+    params.require(:journal_entry).permit(:name, :content, :photo)
+  end
 end
