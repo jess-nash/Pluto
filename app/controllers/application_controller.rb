89b44a2fb0c532fa5a5f9b387f630a27ec49d1ee
Jessica Nash <jessicanashacc@gmail.com>
2022-08-11 20:53:08 +0900

    pet policygit statusgit status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit
+  include Pundit::Authorization
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
