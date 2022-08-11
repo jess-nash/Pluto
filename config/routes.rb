3e9b1afe785c814dd116e86e690d482fa1b7f5cb
Jessica Nash <jessicanashacc@gmail.com>
2022-08-11 19:54:01 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :pets do
+  resources :pets, only: [:index, :show] do
+    get "/profile", to: "pets#profile", as: :profile
     resources :journal_entries, only: [:index, :new, :create]
   end
 
