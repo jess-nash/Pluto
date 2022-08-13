ac669c1b93e834d67735398babd79d057dd5f584
Jessica Nash <jessicanashacc@gmail.com>
2022-08-13 11:23:44 +0900

    fix conflict in routeS

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-<<<<<<< HEAD
-  resources :pets do
-    resources :journal_entries, only: [:index, :new, :create]
-  end
-
-=======
   resources :pets, only: [:index, :show] do
     get "/profile", to: "pets#profile", as: :profile
     resources :journal_entries, only: [:index, :new, :create]
   end
->>>>>>> main
 end
