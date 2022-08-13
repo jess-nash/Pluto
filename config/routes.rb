7dde62c1afeaefe6ee220d106850319698ebaca0
Nicole Olliver <noliver.ykc@gmail.com>
2022-08-13 15:32:00 +0900

    pet profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   resources :pets, only: [:index, :show] do
     resources :journal_entries, only: [:index, :new, :create]
+
   end
   # yann said remove get from resources
-  get "/profile", to: "pets#profile", as: :profile
+  get "/pets/:pet_id/profile", to: "pets#profile", as: :profile
 end
