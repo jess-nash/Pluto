cdb6c7ca94a8e3ba9ccf3d00f7dd236a6f27211c
Nicole Olliver <noliver.ykc@gmail.com>
2022-08-15 15:11:51 +0900

    ordered journal entries newest first

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,6 +1,6 @@
 class JournalEntriesController < ApplicationController
   def index
-    @journals = policy_scope(JournalEntry).order(created_at: :desc)
+    @journals = policy_scope(JournalEntry)
     @journals_as_owner = current_user.journal_entries_as_owner.order(created_at: :desc)
     @pet = Pet.find(params[:pet_id])
   end
