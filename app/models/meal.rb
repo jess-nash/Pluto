b1462ce279eee6be0d7385c204a4a41339c30d6b
Kenneth Ashmore <kyashmore91@gmail.com>
2022-08-16 21:40:54 +0900

    created seeds for meals and added pet to them

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,8 +2,8 @@ class Meal < ApplicationRecord
   belongs_to :pet
 
   validates :meal_type, presence: true, length: { in: 2..30 }
-  validates :description, length: { in: 2..50 }
-  validates :time, length: { in: 2..50 }
+  validates :description, length: { in: 2..150 }
+  validates :time, length: { in: 1..50 }
 
   has_many_attached :photos
 end
