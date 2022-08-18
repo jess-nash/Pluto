6bd4f02bf303d97ffd9ca248986e3985086f3125
Tyler Hennig <tylerjhennig@gmail.com>
2022-08-18 21:29:20 +0900

    new medicines form!

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,2 +1,28 @@
 class MedicinesController < ApplicationController
+  def new
+    @medicine = Medicine.new
+    @pet = Pet.find(params[:pet_id])
+    authorize @medicine
+  end
+
+  def create
+    @medicine = Medicine.new(medicine_params)
+    @pet = Pet.find(params[:pet_id])
+    @medicine.pet = @pet
+    authorize @medicine
+
+    if @medicine.save
+      redirect_to pet_medicine_path, notice: 'New medicine was added!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def medicine_params
+    params.require(:medicine).permit(
+      :name, :dosage, :start_date, :end_date, :expiration_date, :description, :medicine_type, photos: []
+    )
+  end
 end
