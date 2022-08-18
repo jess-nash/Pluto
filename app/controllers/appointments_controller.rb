e574cd7bcc1df0ca310d4112a27d52cc567fc996
Nicole Olliver <noliver.ykc@gmail.com>
2022-08-18 20:04:08 +0900

    created appointment routes, controller and policy

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,7 @@
 class AppointmentsController < ApplicationController
+  def index
+    @appointments = policy_scope(Appointment)
+    @pet = Pet.find(params[:pet_id])
+    @appointments = @pet.appointments.order(created_at: :desc)
+  end
 end
