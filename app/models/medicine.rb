6e20196dacaed0cb5356a1dfa0d501b77aeb4c44
Jessica Nash <jessicanashacc@gmail.com>
2022-08-25 20:41:09 +0900

    changed min to 1 of dosage and description

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -2,8 +2,8 @@ class Medicine < ApplicationRecord
   belongs_to :pet
 
   validates :name, presence: true, length: { in: 2..30 }
-  validates :dosage, presence: true, length: { in: 2..100 }
-  validates :description, length: { in: 2..1_000 }
+  validates :dosage, presence: true, length: { in: 1..100 }
+  validates :description, length: { in: 0..1_000 }
 
   has_many_attached :photos
 end
