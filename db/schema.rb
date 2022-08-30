1d316c5c704bee200bdcbf4c1cd6d76949fbdbcb
Nicole Olliver <noliver.ykc@gmail.com>
2022-08-30 23:08:38 +0900

    fixed the seeds for journals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_111130) do
+ActiveRecord::Schema.define(version: 2022_08_27_074222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
