24970a146864a3458de1d129db0443347ddb8810
Nicole Olliver <noliver.ykc@gmail.com>
2022-09-02 16:15:14 +0900

    edited extra page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
-  before_action :check_signed_in
+  # before_action :check_signed_in
 
   def check_signed_in
     redirect_to pets_path if signed_in?
